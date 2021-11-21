639b2da9d617cb76dfea1f8a86b76ef5e0095dc4
Maricaya <915270549@qq.com>
2021-11-21 10:20:20 +0800

    swc

--- webpack.config.js
+++ webpack.config.js
@@ -1,45 +1,63 @@
-const path = require('path');
+const {resolve} = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+// const merge = require('webpack-merge');
+// const argv = require('yargs-parser')(process.argv.slice(2))
+// const _mode = argv.mode || 'development';
+// const _mergeConfig = require(`./config/webpack.${_mode}.js`)
+// const webpackBar = require('webpackbar');
 
 module.exports = {
-    entry: './src/index',
-    module: {
-        rules: [
-            {
-                test: /\.tsx?$/,
-                use: 'ts-loader',
-                exclude: /node_modules/,
-            },
-            {
-                test: /\.css$/i,
-                use: ['style-loader', 'css-loader'],
-            },
-            {
-                test: /\.(png|jpe?g|gif)$/i,
-                use: [
-                    {
-                        loader: 'file-loader',
-                    },
-                ],
-            },
-            {
-                test: /\.(woff|woff2|eot|ttf|otf)$/i,
-                type: 'asset/resource',
-            },
+  entry:
+    './src/index',
+  // another: './src/module'
+  module: {
+    rules: [
+      // {
+      //   test: /\.tsx?$/,
+      //   use: 'ts-loader',
+      //   exclude: /node_modules/,
+      // },
+      {
+        test: /\.([jt]sx?)?$/,
+        use: "swc-loader",
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        use: [
+          {
+            loader: 'file-loader',
+          },
         ],
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            title: "hello react", // 页面 title
-            template: "index.html"
-        })
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/resource',
+      },
     ],
-    resolve: {
-        extensions: [".ts", ".tsx", ".js", ".jsx"],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "hello react", // 页面 title
+      template: "index.html"
+    })
+  ],
+  resolve: {
+    extensions: [".ts", ".tsx", ".js", ".jsx"],
+  },
+  output: {
+    filename: '[name].bundle.js',
+    path: resolve(__dirname, 'dist'),
+    clean: true,
+  },
+  optimization: {
+    runtimeChunk: 'single',
+    splitChunks: {
+      chunks: 'all',
     },
-    output: {
-        filename: '[name].bundle.js',
-        path: path.resolve(__dirname, 'dist'),
-        clean: true,
-    },
+  },
 };
